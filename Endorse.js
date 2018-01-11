8e7d2d1d2bec236bb24ed4720957de21ee83bbd6
Ray Winkelman <raywinkelman@gmail.com>
2018-01-10 21:33:45 -0400

    Updates to comply with recent UI changes + ES6 reformatting effort.

--- Endorse.js
+++ Endorse.js
@@ -6,27 +6,16 @@
 // @grant       none
 // ==/UserScript==
 
-setTimeout(function(){
-for(var x = 0; x < 9000; x += 500){
-  setTimeout(function(){
-      window.scrollTo(0,document.body.scrollHeight);
-  }, x);
-}
+setTimeout(() => {
+    for(var x = 0; x < 9000; x += 1000){
+        setTimeout(() => {window.scrollTo(0,document.body.scrollHeight);}, x);
+    }
+}, 15000);
 
-}, 5000);
-
-setTimeout(function(){
-    $("button[aria-controls='featured-skills-expanded']").click();
-
-    $("path[d='M21,13H13v8H11V13H3V11h8V3h2v8h8v2Z']").each(function(x, e) {
-
-          if(x < 40)
-          {
-              $(e).click();
-          }
-          else {
-              return;
-          }
+setTimeout(() => {
+    $("button[data-control-name='skill_details']").click();
+    $("button[data-control-name='endorse']").each((x, e) => {
+          if(x < 40){$(e).click();}
+          else {return;}
     });
-
-}, 17500);
+}, 20000);
--- GetConnections.js
+++ GetConnections.js
@@ -1,13 +1,9 @@
+Error.stackTraceLimit = Infinity;
+
 for(var x = 0; x < 89000; x += 500){
-  setTimeout(function(){
-      window.scrollTo(0,document.body.scrollHeight);
-  }, x);
+  setTimeout(() => window.scrollTo(0,document.body.scrollHeight), x);
 }
 
-setTimeout(function(){
-  $("a[data-control-name='profile']").each(function(x, e) {
-    if(x % 2 == 0){
-        console.log(e.href);
-    }
-  });
+setTimeout(() => {
+    $(".mn-person-info__link").each((x, e) => console.log(e.href))
 }, 90000);
